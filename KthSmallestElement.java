8349c842be0ca1294e7aeeb659527ac2181e3d69
Mahitha-pasupuleti <32636127+Mahitha-pasupuleti@users.noreply.github.com>
2022-11-03 17:18:44 +0530

    Add files via upload

---
+++ KthSmallestElement.java
@@ -0,0 +1,79 @@
+import java.io.*;
+import java.util.*;
+class GFG {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		PrintWriter out = new PrintWriter(System.out);
+		int t=sc.nextInt();
+
+		while(t-->0)
+		{
+		    int n=sc.nextInt();
+
+		    int arr[]=new int[n];
+
+		    for(int i=0;i<n;i++)
+		    arr[i]=sc.nextInt();
+
+		    int k=sc.nextInt();
+		    Solution ob = new Solution();
+		    out.println(ob.kthSmallest(arr, 0, n-1, k));
+		}
+		out.flush();
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    public static int kthSmallest(int[] arr, int l, int r, int k)
+    {
+        /*
+        BRUTE FORCE APPROACH
+        This code segment iterators alot if k is a large element hence TIME LIMIT EXCEEDED exception */
+
+        // int min = 0;
+        // int x = 0;
+        // while(k!=0) {
+        //     min = 999999999;
+        //     for(int i=0; i<=r; i++) {
+        //         if(arr[i]!=-1 && min>arr[i]) {
+        //             min = arr[i];
+        //             x = i;
+        //         }
+        //     }
+        //     arr[x] = -1;
+        //     k--;
+        // }
+
+
+        /*
+        OPTIMIZED APPROACH
+        STEPS
+        1) Use a Set data structure to store array values in a sorted order.
+        2) Use Iterator to iterate the set data structure till k times, to get the kth smallest element.
+         */
+
+        /*
+        Alternative use merge sort to sort the array and return the (k-1)th array element
+         */
+
+        Set<Integer> a = new TreeSet<Integer>();
+        for(int i=0; i<=r; i++) {
+            a.add(arr[i]);
+        }
+
+        Iterator<Integer> itr = a.iterator();
+
+        int min = 0;
+        while(k!=0) {
+            min = itr.next();
+            k--;
+        }
+
+        return min;
+    }
+}
